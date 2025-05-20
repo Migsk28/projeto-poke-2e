d409f107a8b318a0ce3a00bf32c50acc50775b63
Migsk28 <rm95201@estudante.fieb.edu.br>
2025-05-20 11:26:26 +0000

    flex

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import {Routes, Route} from 'react-router-dom'
 import Home from './Home'
+import PokeFlex from './PokeFlex'
+import PokeGrid from './PokeGrid'
 
 const App = () => {
 
   return (
     <Routes>
+      <Route path="pokeflex" element={<PokeFlex/>} />
+      <Route path="pokegrid" element={<PokeGrid/>} />
       <Route path="/" element={<Home/>} />
     </Routes>
   )
